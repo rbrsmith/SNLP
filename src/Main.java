4a45567a338bf58ee1ee193fdadda6ec97c8121c
Ross Smith <rbrsmith@gmail.com>
2016-03-08 09:38:26 -0500

    Test of reading emojis

--- src/Main.java
+++ src/Main.java
@@ -43,13 +43,40 @@ public class Main {
         CharacterModel model = new CharacterModel(training, testing, modelSize,  delta);
 
 //        // Train and then test the model on the two tweet files
-        model.train();
-        model.test();
+//        model.train();
+ //       model.test();
 
 
-        PrintWriter writer = new PrintWriter("/home/ross/Dropbox/IdeaProjects/SMLP/test.txt", "UTF-8");
+        PrintWriter writer = new PrintWriter("C:\\Users\\b0467851\\WORK\\school\\Simplified-TweetLID-corpus\\Simplified-TweetLID-corpus\\test.txt", "UTF-8");
+
+
+        byte[] bytes = {
+                0x48, 0x69, 0x2c,                       // ASCII chars are 1 byte each
+                (byte) 0xe6, (byte) 0x82, (byte) 0xa8,  // U+60A8
+
+                (byte) 0xF0, (byte) 0x9F, (byte) 0x98, (byte) 0x81,
+
+                (byte) 0xe5, (byte) 0xa5, (byte) 0xbd,  // U+597D
+                0x21
+
+        };
+        String s = new String(bytes, "UTF-8");
+
         writer.print("\uD83D\uDE01");
+        writer.println();
+        writer.print(s);
         writer.close();
+        System.out.println(s);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<s.length();) {
+            int codePoint = s.codePointAt(i);
+            System.out.println(codePoint);
+            sb.appendCodePoint(codePoint);
+            System.out.println(Character.isValidCodePoint(codePoint));
+            i += Character.charCount(codePoint);
+        }
+        System.out.println(sb);
+
     }
 
 
